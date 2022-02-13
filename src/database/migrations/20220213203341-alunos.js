002b21b1365f08ec80dd1810d318f66a1358f828
Caique Lins <caique.c.lins@gmail.com>
2022-02-13 18:24:17 -0300

    Configurando e testando o Sequelize

---
+++ src/database/migrations/20220213203341-alunos.js
@@ -0,0 +1,46 @@
+module.exports = {
+  // eslint-disable-next-line no-unused-vars
+  up: (queryInterface, Sequelize) => queryInterface.createTable('alunos', {
+    id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      autoIncrement: true.valueOf,
+      primaryKey: true,
+    },
+    nome: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    sobrenome: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    idade: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    peso: {
+      type: Sequelize.FLOAT,
+      allowNull: false,
+    },
+    altura: {
+      type: Sequelize.FLOAT,
+      allowNull: false,
+    },
+    created_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    updated_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+  }),
+
+  // eslint-disable-next-line no-unused-vars
+  down: (queryInterface) => queryInterface.dropTable('alunos'),
+};
