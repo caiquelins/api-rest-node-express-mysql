46ff126faf432687cc9f4930662b696a31bc1ae5
Caique Lins <caique.c.lins@gmail.com>
2022-02-14 15:33:45 -0300

    Criando Usuários

---
+++ migrations/20220214172750-users.js
@@ -0,0 +1,35 @@
+module.exports = {
+  // eslint-disable-next-line no-unused-vars
+  up: (queryInterface, Sequelize) => queryInterface.createTable('users', {
+    id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      autoIncrement: true.valueOf,
+      primaryKey: true,
+    },
+    nome: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password_hash: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    created_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    updated_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+  }),
+
+  // eslint-disable-next-line no-unused-vars
+  down: (queryInterface) => queryInterface.dropTable('users'),
+};
